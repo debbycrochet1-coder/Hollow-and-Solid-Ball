458dd76f5e98f4ddfd883e70b868a2afc00bcb03
Debbie <debbycrochet1@gmail.com>
2026-09-19 14:37:16 +0100

    Add files via upload

---
+++ main2.py
@@ -0,0 +1,20 @@
+import pygame
+pygame.init()
+
+screen= pygame.display.set_mode((400,400))
+screen.fill((255,255,255))
+GREEN= (0,255,0)
+
+pygame.draw.circle(screen, GREEN, (100,100), 50)
+
+pygame.draw.circle(screen, (0,0,255), (300,300), 50, 3)
+
+pygame.display.update()
+
+running= True
+while running:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running= False
+
+pygame.quit()
